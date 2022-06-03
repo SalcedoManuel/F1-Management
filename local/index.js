38e5c36beb4400ba2d00206412f53c5a7009ef78
SalcedoManuel <m.salcedoa.2016@alumnos.urjc.es>
2022-06-04 01:46:40 +0200

    Inicio del JS

---
+++ local/index.js
@@ -0,0 +1,30 @@
+console.log("Ejecutando Javascript...");
+
+let race = document.getElementsByClassName("Laps");
+for(var i = 0; i < race.length; i++){
+    race[i].onclick = (ev)=>{
+        length_race(ev.target);
+    }
+};
+function length_race(boton){
+    var race_length = boton.value;
+    console.log(race_length);
+}
+
+let clima = document.getElementsByClassName("Weather");
+for (let i = 0; i < clima.length; i++) {
+    clima[i].onclick = (ev)=>{
+        set_weather(ev.target);
+    }
+    
+}
+function set_weather(boton){
+    var weather = boton.value;
+    console.log(weather);
+}
+
+
+/* Cálculo de las estrategias más óptimas para el gran premio*/
+calculo.onclick = () =>{
+    
+}
